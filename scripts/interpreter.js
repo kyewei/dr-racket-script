9905870563d3df57fe4e33a1888afcef271e4130
kyewei <kyewei22@yahoo.ca>
2014-11-24 00:10:19 -0500

    Implemented recursive block parser
    Checks for correctly-paired brackets, does not error check yet

--- scripts/interpreter.js
+++ scripts/interpreter.js
@@ -70,6 +70,9 @@ function parseStr(strArr) {
         parsedStrCodeTree[i] = recursivelyBuildCodeTree(strCodeBlocks[i]);
     }
     
+    console.log("Parsed String Tree:" );
+    console.log(parsedStrCodeTree);
+    
     return strCodeBlocks;
 };
 
@@ -78,17 +81,17 @@ function recognizeBlock(unparsedBlocks) {
         return unparsedBlocks;
     block = [];
     while (unparsedBlocks.length > 0){
-        if (unparsedBlocks[0]=="(" || unparsedBlocks[0] == "[") {
-            var blockstart = unparsedBlocks[0];
+        if (unparsedBlocks[0]==="(" || unparsedBlocks[0] === "[") {
+            var blockStart = unparsedBlocks[0];
             var bracketcount = 1;
             for (var i=1; i<unparsedBlocks.length; ++i) {
-                if (unparsedBlocks[i]=="(" || unparsedBlocks[i] == "[")
+                if (unparsedBlocks[i]==="(" || unparsedBlocks[i] === "[")
                     bracketcount++;
-                else if (unparsedBlocks[i]==")" || unparsedBlocks[i] == "]")
+                else if (unparsedBlocks[i]===")" || unparsedBlocks[i] === "]") {
                     bracketcount--;
                     if (bracketcount ==0) {
-                        if ( (blockstart == "[" && unparsedBlocks[i] == "]") ||
-                             (blockstart == "(" && unparsedBlocks[i] == ")") ){
+                        if ((blockStart === "[" && unparsedBlocks[i] === "]") ||
+                        (blockStart === "(" && unparsedBlocks[i] === ")")) {
                             var splicedblock = unparsedBlocks.slice(0,i+1);
                             block.push(splicedblock);
                             unparsedBlocks = unparsedBlocks.slice(i+1,unparsedBlocks.length);
@@ -100,16 +103,94 @@ function recognizeBlock(unparsedBlocks) {
                             unparsedBlocks=[];
                         }
                     }
+                }
             }
         }
         else
             console.log("Missing brackets");     
     }
     return block;
-}
+};
 
+// strBlock is Array of (String)
+// returns Array of (String or (Array of (String)))
 function recursivelyBuildCodeTree(strBlock) {
-
+    console.log("recursivelyBuildCodeTree called with: ");
+    console.log(strBlock);
+    if (strBlock.length>0) {
+        var subBlocks = [];
+        var startIndex = 0;
+        var bracketCount = 0;
+        
+        //initial brackets
+        if (strBlock[0] =="(" || strBlock[0] =="[" ) {
+            startIndex = 1;
+            bracketCount++;
+            
+            //tracking expressions
+            for (var i=1; i< strBlock.length-1; ++i) {
+                //subexpression
+                if (strBlock[i] ==="(" || strBlock[i] === "[") {
+                    var blockStart = strBlock[i];
+                    
+                    console.log("Subexpression!");
+                    bracketCount++;
+                    
+                    var closedBracketCount = bracketCount-1;
+                    
+                    //searches for closing bracket, then slices and recurses
+                    for (var j=i+1; j< strBlock.length; ++j) {
+                        console.log("i:"+ i + ", j:" +j);
+                        if (strBlock[j] ==="(" || strBlock[j] === "[")
+                            bracketCount++;
+                        else if (strBlock[j] ===")" || strBlock[j] === "]"){
+                            bracketCount--;
+                            if (closedBracketCount == bracketCount) {
+                                if ((blockStart === "[" && strBlock[j] === "]") ||
+                                (blockStart === "(" && strBlock[j] === ")")) {
+                                    console.log("Found closing bracket!");
+                                    var subExpression = recursivelyBuildCodeTree(strBlock.slice(startIndex,j+1));
+                                    subBlocks.push(subExpression);
+                                    startIndex =j+1;
+                                    i=j; //i++ is done everytime
+                                    j = strBlock.length; //exit inner loop
+                                }
+                                else 
+                                    console.log("Mis-paired bracket!");
+                            }
+       
+                        }
+                    }
+                }
+                else if (strBlock[j] ===")" || strBlock[j] === "]"){
+                    bracketCount--;
+                }
+                //singleton, directly add to exp tree
+                else {
+                    var singleton = strBlock.slice(startIndex,i+1)[0];
+                    console.log("Singleton: ");
+                    console.log(singleton);
+                    subBlocks.push(singleton);
+                    startIndex=i+1;
+                }
+            }
+        }
+        // decrement for closing brackets here        
+        else if (strBlock[j] ===")" || strBlock[j] === "]"){
+            bracketCount--;
+        }
+        else {
+            console.log("Bracket-less array code block!");
+        }
+        return subBlocks;
+    }
+    else if (strBlock.length == 0)
+        //should not be brackets
+        return strBlock[0];
+    else 
+        console.log("Null element!!!");
+        return null;
+    
 };
 
 
